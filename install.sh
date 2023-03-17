a4e28893b599875b088465ca20ab5aabe321db20
voidfemme <rose.proctor@pcc.edu>
2023-03-17 12:04:22 -0700

    cleaned up files and edited the install script

--- install.sh
+++ install.sh
@@ -13,15 +13,35 @@ if [ "$(id -u)" -ne 0 ]; then
 fi
 
 # Copy the program to /usr/local/bin and set the executable permission
+echo "Copying program..."
 cp "$program_file" "/usr/local/bin/$program_name"
 chmod +x "/usr/local/bin/$program_name"
 
 # Copy the man page to /usr/local/share/man/man1 and set the read permission
+echo "Copying man page..."
 mkdir -p "/usr/local/share/man/man1"
-cp "$man_page" "/usr/local/share/man/man1/"
+cp "docs/$man_page" "/usr/local/share/man/man1/"
 chmod +r "/usr/local/share/man/man1/$man_page"
 
 # Update the man database
-mandb
+echo "Updating man database..."
+mandb -q
 
-echo "Installation complete. You can now run 'chatbash' from the command line"
+# Check if the program file installation was successful
+echo "Checking program file installation..."
+if [ -x "/usr/local/bin/$program_name" ]; then
+    echo "Program file installed successfully."
+else
+    echo "Program file installation failed. Please check the script and try again."
+    exit 1 
+fi
+
+# Check if the man page file installation was successful
+echo "Checking man page file installation..."
+if [ -r "/usr/local/share/man/man1/$man_page" ]; then
+    echo "Man page file installed successfully. You can now run 'chatbash' from the command line."
+else
+    echo "Man page file installation failed. Please check the script and try again."
+    exit 1 
+fi
+
